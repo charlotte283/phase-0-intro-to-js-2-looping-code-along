74959eba206241eb95584867870e5561b60de480
charlotte karimi <[charlotte.karimi@student.moringaschool.com]>
2022-12-15 20:21:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+let cards = ["Guadalupe","Ollie","Aki"]
+
+function writeCards(cards) {
+    for(let i =0;i<cards.length;i++)
+    return `Thank you, ${cards[i]},for the wonderful surprise gift!`
+}
+
+let countDown = 5
+while (countDown<5){
+    console.log (countDown--);
+}
